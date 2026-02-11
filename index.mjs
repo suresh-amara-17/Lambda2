d66fd48de616fa3dcfa92c36ea4201c2d61ebfbb
venugopl <venugopal.batthina@gmail.com>
2026-02-11 16:38:27 +0530

    second commit

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda! Github'),
+    body: JSON.stringify('Hello from Lambda! Github '),
   };
   return response;
 };
